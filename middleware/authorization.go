1a0bc83bbce4e896bd5263c4780b5b8b1b311fb6
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-06-24 09:53:49 -0300

    fix: authorization handler

--- middleware/authorization.go
+++ middleware/authorization.go
@@ -8,12 +8,12 @@ import (
 
 func Authorization(role string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		claims, ok := c.Get(IDENTITY_KEY)
-		if !ok || claims.(*UserClaims).Role != role {
+		if claims, ok := c.Get(IDENTITY_KEY); !ok || claims.(*UserClaims).Role != role {
 			c.JSON(http.StatusForbidden, gin.H{
 				"code":    http.StatusForbidden,
 				"message": "permission denied.",
 			})
+			c.AbortWithStatus(http.StatusForbidden)
 		}
 	}
 }
