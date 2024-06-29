35231eb8ee9bb6ba3fa63deb0507b129be89f388
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-06-29 03:36:51 -0300

    feat: add permission to super admin to all routes

--- middleware/authorization.go
+++ middleware/authorization.go
@@ -4,11 +4,12 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/wesleyfebarretos/ticket-sale/internal/enum"
 )
 
 func Authorization(role string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if claims, ok := c.Get(IDENTITY_KEY); !ok || claims.(*UserClaims).Role != role {
+		if claims, ok := c.Get(IDENTITY_KEY); !ok || claims.(*UserClaims).Role != role && claims.(*UserClaims).Role != enum.SUPER_ADMIN {
 			c.JSON(http.StatusForbidden, gin.H{
 				"code":    http.StatusForbidden,
 				"message": "permission denied.",
