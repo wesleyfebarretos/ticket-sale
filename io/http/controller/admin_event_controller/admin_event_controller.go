46be103b53f3123361ddbc5705d64150a0b7a889
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-07-05 19:17:51 -0300

    docs: admin event swagger

--- io/http/controller/admin_event_controller/admin_event_controller.go
+++ io/http/controller/admin_event_controller/admin_event_controller.go
@@ -14,6 +14,19 @@ import (
 	"github.com/wesleyfebarretos/ticket-sale/repository/admin_products_repository"
 )
 
+// CreateEvent godoc
+//
+//	@Tags			Admin Event
+//	@Summary		Create a event
+//	@Description	Create a event
+//	@Produce		json
+//	@Param			Event	body		CreateRequestDto	true	"New Event"
+//	@Success		201		{object}	CreateResponseDto
+//	@Failure		500		{object}	exception.HttpException
+//	@Failure		400		{object}	exception.HttpException
+//	@Failure		403		{object}	middleware.RolePermissionError
+//	@Failure		401		{object}	middleware.AuthenticationError
+//	@Router			/admin/events [post]
 func Create(c *gin.Context) {
 	adminUserClaims := controller.GetClaims(c)
 
@@ -87,6 +100,21 @@ func Create(c *gin.Context) {
 	c.JSON(http.StatusCreated, newEventRes)
 }
 
+// UpdateEvent godoc
+//
+//	@Tags			Admin Event
+//	@Summary		Update a event
+//	@Description	Update a event
+//	@Produce		json
+//	@Param			id		path		int					true	"Event ID"
+//	@Param			Event	body		UpdateRequestDto	true	"Update Event"
+//	@Success		200		{object}	bool
+//	@Failure		500		{object}	exception.HttpException
+//	@Failure		404		{object}	exception.HttpException
+//	@Failure		400		{object}	exception.HttpException
+//	@Failure		403		{object}	middleware.RolePermissionError
+//	@Failure		401		{object}	middleware.AuthenticationError
+//	@Router			/admin/events/{id} [put]
 func Update(c *gin.Context) {
 	id := controller.GetId(c)
 	adminUserClaims := controller.GetClaims(c)
@@ -122,6 +150,19 @@ func Update(c *gin.Context) {
 	c.JSON(http.StatusOK, true)
 }
 
+// SoftDeleteEvent godoc
+//
+//	@Tags			Admin Event
+//	@Summary		Soft Delete a event
+//	@Description	Soft Delete a event
+//	@Produce		json
+//	@Param			id	path		int	true	"Event ID"
+//	@Success		200	{object}	bool
+//	@Failure		500	{object}	exception.HttpException
+//	@Failure		400	{object}	exception.HttpException
+//	@Failure		403	{object}	middleware.RolePermissionError
+//	@Failure		401	{object}	middleware.AuthenticationError
+//	@Router			/admin/events/{id} [delete]
 func SoftDelete(c *gin.Context) {
 	id := controller.GetId(c)
 
@@ -130,6 +171,18 @@ func SoftDelete(c *gin.Context) {
 	c.JSON(http.StatusOK, true)
 }
 
+// GetAllEvents godoc
+//
+//	@Tags			Admin Event
+//	@Summary		Get all events
+//	@Description	Get all events
+//	@Produce		json
+//	@Success		200	{object}	[]GetAllResponseDto
+//	@Failure		500	{object}	exception.HttpException
+//	@Failure		400	{object}	exception.HttpException
+//	@Failure		403	{object}	middleware.RolePermissionError
+//	@Failure		401	{object}	middleware.AuthenticationError
+//	@Router			/admin/events [get]
 func GetAll(c *gin.Context) {
 	events := admin_event_service.GetAll(c)
 
@@ -147,6 +200,20 @@ func GetAll(c *gin.Context) {
 	c.JSON(http.StatusOK, eventsResponse)
 }
 
+// GetOneById godoc
+//
+//	@Tags			Admin Event
+//	@Summary		Get One By Id
+//	@Description	Get One By Id
+//	@Produce		json
+//	@Param			id	path		int	true	"Event ID"
+//	@Success		200	{object}	GetOneByIdResponseDto
+//	@Failure		500	{object}	exception.HttpException
+//	@Failure		404	{object}	exception.HttpException
+//	@Failure		400	{object}	exception.HttpException
+//	@Failure		403	{object}	middleware.RolePermissionError
+//	@Failure		401	{object}	middleware.AuthenticationError
+//	@Router			/admin/events/{id} [get]
 func GetOneById(c *gin.Context) {
 	id := controller.GetId(c)
 
