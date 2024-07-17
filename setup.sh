f6cab8fa16e68c5a187746055be1319cc1d8198c
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-07-17 00:32:01 -0300

    feat: implement logstash jdbc db connection to extract data example

--- setup.sh
+++ setup.sh
@@ -8,4 +8,7 @@ sudo chown 0:0 ./infra/elastic_stack/filebeat/filebeat.yml
 sudo chmod go-w ./infra/elastic_stack/metricbeat/metricbeat.yml
 sudo chown 0:0 ./infra/elastic_stack/metricbeat/metricbeat.yml
 
+sudo chmod go-w ./infra/elastic_stack/logstash/logstash.conf
+sudo chown 0:0 ./infra/elastic_stack/logstash/logstash.conf
+
 docker compose up -d
