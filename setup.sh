37381645e531ae283b1d13dd4ade7018ca4df510
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-07-16 21:43:00 -0300

    feat: Metricbeat

--- setup.sh
+++ setup.sh
@@ -5,4 +5,7 @@ source .env
 sudo chmod go-w ./infra/elastic_stack/filebeat/filebeat.yml
 sudo chown 0:0 ./infra/elastic_stack/filebeat/filebeat.yml
 
+sudo chmod go-w ./infra/elastic_stack/metricbeat/metricbeat.yml
+sudo chown 0:0 ./infra/elastic_stack/metricbeat/metricbeat.yml
+
 docker compose up -d
