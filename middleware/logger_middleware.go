1e46234fcf0aa62cb1e5b68fec97493793d9da7c
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-07-18 20:51:38 -0300

    fix: logs

--- middleware/logger_middleware.go
+++ middleware/logger_middleware.go
@@ -4,7 +4,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/wesleyfebarretos/ticket-sale/infra/logger"
+	"github.com/wesleyfebarretos/ticket-sale/logger"
 )
 
 func Logger(c *gin.Context) {
