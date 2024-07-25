c687934566f1b2fdb34e47667e524c70a76bdad3
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-07-25 03:59:14 -0300

    style: swagger ident

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,17 +13,20 @@ import (
 	"github.com/wesleyfebarretos/ticket-sale/api/routes"
 )
 
-// @title			Ticket Sale
-// @version		1.0
-// @description	This is a simple ticket sales application.
-// @termsOfService	http://swagger.io/terms/
-// @contact.name	Ticket Sale Support
-// @contact.url	http://www.swagger.io/support
-// @contact.email	support@swagger.io
-// @license.name	Apache 2.0
-// @license.url	http://www.apache.org/licenses/LICENSE-2.0.html
-// @host			localhost:8080
-// @BasePath		/v1
+// Swagger entrypoint godoc
+//
+//	@title			Ticket Sale
+//
+//	@version		1.0
+//	@description	This is a simple ticket sales application.
+//	@termsOfService	http://swagger.io/terms/
+//	@contact.name	Ticket Sale Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+//	@host			localhost:8080
+//	@BasePath		/v1
 func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
