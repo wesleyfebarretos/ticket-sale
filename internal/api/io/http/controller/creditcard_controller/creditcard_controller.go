ed1e5622504cee31ceacf3e9cc9d0662dd1cd82e
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-07-29 12:26:14 -0300

    docs: creditcard endpoints

--- internal/api/io/http/controller/creditcard_controller/creditcard_controller.go
+++ internal/api/io/http/controller/creditcard_controller/creditcard_controller.go
@@ -10,7 +10,18 @@ import (
 	"github.com/wesleyfebarretos/ticket-sale/internal/api/io/http/controller"
 )
 
-// TODO: Implement swagger documentation config and examples in structs
+// GetAllUserCreditcards godoc
+//
+//	@Tags			Creditcard
+//	@Summary		Get all user creditcards
+//	@Description	Get all user creditcards
+//	@Produce		json
+//	@Success		200	{object}	[]GetAllUserCreditcardsResponseDto
+//	@Failure		500	{object}	exception.HttpException
+//	@Failure		400	{object}	exception.HttpException
+//	@Failure		403	{object}	middleware.RolePermissionError
+//	@Failure		401	{object}	middleware.AuthenticationError
+//	@Router			/creditcard/user [get]
 func GetAllUserCreditcards(c *gin.Context) {
 	user := controller.GetClaims(c)
 
@@ -42,6 +53,19 @@ func GetAllUserCreditcards(c *gin.Context) {
 	c.JSON(http.StatusOK, creditcardsResponse)
 }
 
+// CreateCreditCard godoc
+//
+//	@Tags			Creditcard
+//	@Summary		Create a Creditcard
+//	@Description	Create a Creditcard
+//	@Produce		json
+//	@Param			Creditcard	body		CreateRequestDto	true	"New Creditcard"
+//	@Success		201			{object}	CreateResponseDto
+//	@Failure		500			{object}	exception.HttpException
+//	@Failure		400			{object}	exception.HttpException
+//	@Failure		403			{object}	middleware.RolePermissionError
+//	@Failure		401			{object}	middleware.AuthenticationError
+//	@Router			/creditcard [post]
 func Create(c *gin.Context) {
 	user := controller.GetClaims(c)
 
@@ -81,6 +105,21 @@ func Create(c *gin.Context) {
 	c.JSON(http.StatusCreated, newCreditcardResponse)
 }
 
+// UpdateProduct godoc
+//
+//	@Tags			Creditcard
+//	@Summary		Update a creditcard
+//	@Description	Update a creditcard
+//	@Produce		json
+//	@Param			uuid		path		string				true	"Creditcard UUID"
+//	@Param			Creditcard	body		UpdateRequestDto	true	"Update Creditcard"
+//	@Success		200			{object}	bool
+//	@Failure		500			{object}	exception.HttpException
+//	@Failure		404			{object}	exception.HttpException
+//	@Failure		400			{object}	exception.HttpException
+//	@Failure		403			{object}	middleware.RolePermissionError
+//	@Failure		401			{object}	middleware.AuthenticationError
+//	@Router			/creditcard/{uuid} [put]
 func Update(c *gin.Context) {
 	user := controller.GetClaims(c)
 
@@ -108,6 +147,19 @@ func Update(c *gin.Context) {
 	c.JSON(http.StatusOK, true)
 }
 
+// SoftDeleteProduct godoc
+//
+//	@Tags			Creditcard
+//	@Summary		Soft Delete a creditcard
+//	@Description	Soft Delete a creditcard
+//	@Produce		json
+//	@Param			uuid	path		string	true	"Creditcard UUID"
+//	@Success		200		{object}	bool
+//	@Failure		500		{object}	exception.HttpException
+//	@Failure		400		{object}	exception.HttpException
+//	@Failure		403		{object}	middleware.RolePermissionError
+//	@Failure		401		{object}	middleware.AuthenticationError
+//	@Router			/creditcard/{uuid} [delete]
 func SoftDelete(c *gin.Context) {
 	uuid := controller.GetUuid(c)
 
--- internal/api/io/http/controller/creditcard_controller/creditcard_controller_dto.go
+++ internal/api/io/http/controller/creditcard_controller/creditcard_controller_dto.go
@@ -7,60 +7,60 @@ import (
 )
 
 type CreditcardFlag struct {
-	Id          int32   `json:"id"`
-	Name        string  `json:"name"`
-	Description *string `json:"description"`
-	Regex       string  `json:"regex"`
+	Id          int32   `json:"id" example:"2"`
+	Name        string  `json:"name" example:"visa"`
+	Description *string `json:"description" example:"VISA"`
+	Regex       string  `json:"regex" example:"'^4d{5}'"`
 }
 
 type CreditcardType struct {
-	Id   int32  `json:"id"`
-	Name string `json:"name"`
+	Id   int32  `json:"id" example:"1"`
+	Name string `json:"name" example:"Credit"`
 }
 
 type GetAllUserCreditcardsResponseDto struct {
-	Uuid           uuid.UUID      `json:"uuid"`
-	Name           string         `json:"name"`
-	Number         string         `json:"number"`
-	Expiration     time.Time      `json:"expiration"`
-	UserID         int32          `json:"userId"`
-	CreatedAt      time.Time      `json:"createdAt"`
+	Uuid           uuid.UUID      `json:"uuid" example:"767c1587-1ade-4e6a-a9fe-f0e07c83bda9"`
+	Name           string         `json:"name" binding:"required" example:"Testing"`
+	Number         string         `json:"number" binding:"required,min=8" example:"5574723384289379"`
+	Expiration     time.Time      `json:"expiration" binding:"required" example:"2025-01-01T00:00:00Z"`
+	UserID         int32          `json:"userId" example:"1"`
+	CreatedAt      time.Time      `json:"createdAt" example:"2024-01-01T00:00:00Z"`
 	CreditcardFlag CreditcardFlag `json:"creditcardFlag"`
 	CreditcardType CreditcardType `json:"creditcardType"`
 }
 
 type CreateRequestDto struct {
-	Name             string    `json:"name" binding:"required"`
-	Number           string    `json:"number" binding:"required,min=8"`
-	Expiration       time.Time `json:"expiration" binding:"required"`
-	Priority         int32     `json:"priority"`
-	NotifyExpiration bool      `json:"notifyExpiration"`
-	CreditcardTypeID int32     `json:"creditcardTypeId" binding:"required,min=1"`
-	CreditcardFlagID int32     `json:"creditcardFlagId" binding:"required,min=1"`
+	Name             string    `json:"name" binding:"required" example:"Testing"`
+	Number           string    `json:"number" binding:"required,min=8" example:"5574723384289379"`
+	Expiration       time.Time `json:"expiration" binding:"required" example:"2025-01-01T00:00:00Z"`
+	Priority         int32     `json:"priority" example:"1"`
+	NotifyExpiration bool      `json:"notifyExpiration" example:"true"`
+	CreditcardTypeID int32     `json:"creditcardTypeId" binding:"required,min=1" example:"1"`
+	CreditcardFlagID int32     `json:"creditcardFlagId" binding:"required,min=1" example:"1"`
 }
 
 type CreateResponseDto struct {
-	ID               int32     `json:"id"`
-	Uuid             uuid.UUID `json:"uuid"`
-	Name             string    `json:"name"`
-	Number           string    `json:"number"`
-	Expiration       time.Time `json:"expiration"`
-	Priority         int32     `json:"priority"`
-	NotifyExpiration bool      `json:"notifyExpiration"`
-	UserID           int32     `json:"userId"`
-	CreditcardTypeID int32     `json:"creditcardTypeId"`
-	CreditcardFlagID int32     `json:"creditcardFlagId"`
-	IsDeleted        bool      `json:"isDeleted"`
-	CreatedAt        time.Time `json:"createdAt"`
-	UpdatedAt        time.Time `json:"updatedAt"`
+	ID               int32     `json:"id" example:"1"`
+	Uuid             uuid.UUID `json:"uuid" example:"767c1587-1ade-4e6a-a9fe-f0e07c83bda9"`
+	Name             string    `json:"name" binding:"required" example:"Testing"`
+	Number           string    `json:"number" binding:"required,min=8" example:"5574723384289379"`
+	Expiration       time.Time `json:"expiration" binding:"required" example:"2025-01-01T00:00:00Z"`
+	Priority         int32     `json:"priority" example:"1"`
+	NotifyExpiration bool      `json:"notifyExpiration" example:"true"`
+	UserID           int32     `json:"userId" example:"1"`
+	CreditcardTypeID int32     `json:"creditcardTypeId" binding:"required,min=1" example:"1"`
+	CreditcardFlagID int32     `json:"creditcardFlagId" binding:"required,min=1" example:"1"`
+	IsDeleted        bool      `json:"isDeleted" example:"false"`
+	CreatedAt        time.Time `json:"createdAt" example:"2024-01-01T00:00:00Z"`
+	UpdatedAt        time.Time `json:"updatedAt" example:"2024-01-01T00:00:00Z"`
 }
 
 type UpdateRequestDto struct {
-	Name             string    `json:"name" binding:"required"`
-	Number           string    `json:"number" binding:"required,min=8"`
-	Expiration       time.Time `json:"expiration" binding:"required"`
-	Priority         int32     `json:"priority"`
-	NotifyExpiration bool      `json:"notifyExpiration"`
-	CreditcardTypeID int32     `json:"creditcardTypeId" binding:"required,min=1"`
-	CreditcardFlagID int32     `json:"creditcardFlagId" binding:"required,min=1"`
+	Name             string    `json:"name" binding:"required" example:"Testing Update"`
+	Number           string    `json:"number" binding:"required,min=8" example:"6643213384289379"`
+	Expiration       time.Time `json:"expiration" binding:"required" example:"2025-01-01T00:00:00Z"`
+	Priority         int32     `json:"priority" example:"2"`
+	NotifyExpiration bool      `json:"notifyExpiration" example:"true"`
+	CreditcardTypeID int32     `json:"creditcardTypeId" binding:"required,min=1" example:"2"`
+	CreditcardFlagID int32     `json:"creditcardFlagId" binding:"required,min=1" example:"2"`
 }
