77b13d52640bff36effef1d71ccf1c7ebace9050
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-08-02 12:35:45 -0300

    docs: admin gateway

--- internal/api/io/http/controller/admin_gateway_controller/admin_gateway_controller.go
+++ internal/api/io/http/controller/admin_gateway_controller/admin_gateway_controller.go
@@ -8,6 +8,19 @@ import (
 	"github.com/wesleyfebarretos/ticket-sale/internal/api/io/http/controller"
 )
 
+// CreateGateway godoc
+//
+//	@Tags			Admin Gateway
+//	@Summary		Create a gateway
+//	@Description	Create a gateway
+//	@Produce		json
+//	@Param			Gateway	body		CreateReqDTO	true	"New Gateway"
+//	@Success		201		{object}	CreateResDTO
+//	@Failure		500		{object}	exception.HttpException
+//	@Failure		400		{object}	exception.HttpException
+//	@Failure		403		{object}	middleware.RolePermissionError
+//	@Failure		401		{object}	middleware.AuthenticationError
+//	@Router			/admin/gateway [post]
 func Create(c *gin.Context) {
 	adminUser := controller.GetClaims(c)
 
@@ -22,6 +35,21 @@ func Create(c *gin.Context) {
 	c.JSON(http.StatusCreated, res.FromDomain(gateway))
 }
 
+// UpdateGateway godoc
+//
+//	@Tags			Admin Gateway
+//	@Summary		Update a gateway
+//	@Description	Update a gateway
+//	@Produce		json
+//	@Param			id		path		int				true	"Gateway ID"
+//	@Param			Gateway	body		UpdateReqDTO	true	"Update gateway"
+//	@Success		200		{object}	bool
+//	@Failure		500		{object}	exception.HttpException
+//	@Failure		404		{object}	exception.HttpException
+//	@Failure		400		{object}	exception.HttpException
+//	@Failure		403		{object}	middleware.RolePermissionError
+//	@Failure		401		{object}	middleware.AuthenticationError
+//	@Router			/admin/gateway/{id} [put]
 func Update(c *gin.Context) {
 	adminUser := controller.GetClaims(c)
 
@@ -34,6 +62,19 @@ func Update(c *gin.Context) {
 	c.JSON(http.StatusOK, res)
 }
 
+// SoftDeleteGateway godoc
+//
+//	@Tags			Admin Gateway
+//	@Summary		Soft Delete a gateway
+//	@Description	Soft Delete a gateway
+//	@Produce		json
+//	@Param			id	path		int	true	"Gateway ID"
+//	@Success		200	{object}	bool
+//	@Failure		500	{object}	exception.HttpException
+//	@Failure		400	{object}	exception.HttpException
+//	@Failure		403	{object}	middleware.RolePermissionError
+//	@Failure		401	{object}	middleware.AuthenticationError
+//	@Router			/admin/gateway/{id} [delete]
 func SoftDelete(c *gin.Context) {
 	adminUser := controller.GetClaims(c)
 
@@ -49,6 +90,18 @@ func SoftDelete(c *gin.Context) {
 	c.JSON(http.StatusOK, res)
 }
 
+// GetAllGateways godoc
+//
+//	@Tags			Admin Gateway
+//	@Summary		Get all gateway
+//	@Description	Get all gateway
+//	@Produce		json
+//	@Success		200	{object}	[]GetAllResDTO
+//	@Failure		500	{object}	exception.HttpException
+//	@Failure		400	{object}	exception.HttpException
+//	@Failure		403	{object}	middleware.RolePermissionError
+//	@Failure		401	{object}	middleware.AuthenticationError
+//	@Router			/admin/gateway [get]
 func GetAll(c *gin.Context) {
 
 	gateways := admin_gateway_service.GetAll(c)
@@ -58,6 +111,20 @@ func GetAll(c *gin.Context) {
 	c.JSON(http.StatusOK, res.FromDomain(gateways))
 }
 
+// GetOneById godoc
+//
+//	@Tags			Admin Gateway
+//	@Summary		Get One By Id
+//	@Description	Get One By Id
+//	@Produce		json
+//	@Param			id	path		int	true	"Gateway ID"
+//	@Success		200	{object}	GetOneByIdResDTO
+//	@Failure		500	{object}	exception.HttpException
+//	@Failure		404	{object}	exception.HttpException
+//	@Failure		400	{object}	exception.HttpException
+//	@Failure		403	{object}	middleware.RolePermissionError
+//	@Failure		401	{object}	middleware.AuthenticationError
+//	@Router			/admin/gateway/{id} [get]
 func GetOneById(c *gin.Context) {
 	id := controller.GetId(c)
 
--- internal/api/io/http/controller/admin_gateway_controller/dto.go
+++ internal/api/io/http/controller/admin_gateway_controller/dto.go
@@ -27,13 +27,13 @@ type CreateReqDTO struct {
 	UseAntifraud     bool    `json:"useAntifraud" example:"true"`
 	CreatedBy        int32   `json:"-"`
 	UpdatedBy        *int32  `json:"-"`
-	PaymentTypes     []int32 `json:"paymentTypes" binding:"required,min=1,dive" example:"[1]"`
+	PaymentTypes     []int32 `json:"paymentTypes" binding:"required,min=1,dive" example:"1,2"`
 }
 
 type CreatePaymentTypeResDTO struct {
-	ID                   int32     `json:"id"`
-	GatewayID            int32     `json:"gatewayId"`
-	GatewayPaymentTypeID int32     `json:"gatewayPaymentTypeId"`
+	ID                   int32     `json:"id" example:"1"`
+	GatewayID            int32     `json:"gatewayId" example:"1"`
+	GatewayPaymentTypeID int32     `json:"gatewayPaymentTypeId" example:"1"`
 	CreatedBy            int32     `json:"-"`
 	UpdatedBy            *int32    `json:"-"`
 	CreatedAt            time.Time `json:"-"`
@@ -41,31 +41,31 @@ type CreatePaymentTypeResDTO struct {
 }
 
 type CreateResDTO struct {
-	ID               int32                     `json:"id"`
-	Uuid             uuid.UUID                 `json:"uuid"`
-	Name             string                    `json:"name"`
-	Description      *string                   `json:"description"`
-	ClientID         *string                   `json:"clientId"`
-	ClientSecret     *string                   `json:"clientSecret"`
-	Order            int32                     `json:"order"`
-	Active           bool                      `json:"active"`
-	IsDeleted        bool                      `json:"isDeleted"`
-	TestEnvironment  bool                      `json:"testEnvironment"`
-	NotifUser        *string                   `json:"notifUser"`
-	NotifPassword    *string                   `json:"notifPassword"`
-	SoftDescriptor   *string                   `json:"softDescriptor"`
-	GatewayProcessID int32                     `json:"gatewayProcessId"`
-	WebhookUrl       *string                   `json:"webhookUrl"`
-	Url              *string                   `json:"url"`
-	AuthType         string                    `json:"authType"`
-	Use3ds           bool                      `json:"use3ds"`
-	AdqCode3ds       *string                   `json:"adqCode3ds"`
-	DefaultAdqCode   *string                   `json:"defaultAdqCode"`
-	UseAntifraud     bool                      `json:"useAntifraud"`
+	ID               int32                     `json:"id" example:"1"`
+	Uuid             uuid.UUID                 `json:"uuid" example:"78e5259f-1b4e-460c-90d2-83e640a7d024"`
+	Name             string                    `json:"name" example:"Stripe"`
+	Description      *string                   `json:"description" example:"Payment gateway for Stripe"`
+	ClientID         *string                   `json:"clientId" example:"ZYDPLLBWSK3MVQJSIYHB1OR2JXCY0X2C5UJ2QAR2MAAIT5Q"`
+	ClientSecret     *string                   `json:"clientSecret" example:"52caa7c6-d107-42bd-afac-0226a501e66e"`
+	Order            int32                     `json:"order" example:"1"`
+	Active           bool                      `json:"active" example:"true"`
+	TestEnvironment  bool                      `json:"testEnvironment" example:"false"`
+	NotifUser        *string                   `json:"notifUser" example:"notification_user"`
+	NotifPassword    *string                   `json:"notifPassword" example:"notification_password"`
+	SoftDescriptor   *string                   `json:"softDescriptor" example:"Company Name"`
+	GatewayProcessID int32                     `json:"gatewayProcessId" example:"1"`
+	WebhookUrl       *string                   `json:"webhookUrl" example:"https://example.com/webhook"`
+	Url              *string                   `json:"url" example:"https://api.stripe.com"`
+	AuthType         string                    `json:"authType" example:"bearer"`
+	Use3ds           bool                      `json:"use3ds" example:"false"`
+	AdqCode3ds       *string                   `json:"adqCode3ds" example:"123456"`
+	DefaultAdqCode   *string                   `json:"defaultAdqCode" example:"654321"`
+	UseAntifraud     bool                      `json:"useAntifraud" example:"true"`
+	IsDeleted        bool                      `json:"isDeleted" example:"false"`
 	CreatedBy        int32                     `json:"-"`
 	UpdatedBy        *int32                    `json:"-"`
-	CreatedAt        time.Time                 `json:"createdAt"`
-	UpdatedAt        time.Time                 `json:"updatedAt"`
+	CreatedAt        time.Time                 `json:"createdAt" example:"2023-01-01T00:00:00Z"`
+	UpdatedAt        time.Time                 `json:"updatedAt" example:"2023-01-01T00:00:00Z"`
 	PaymentTypes     []CreatePaymentTypeResDTO `json:"paymentTypes"`
 }
 
@@ -91,59 +91,59 @@ type UpdateReqDTO struct {
 }
 
 type GetAllResDTO struct {
-	ID                  int32                    `json:"id"`
-	Uuid                uuid.UUID                `json:"uuid"`
-	Name                string                   `json:"name"`
-	Description         *string                  `json:"description"`
-	ClientID            *string                  `json:"clientId"`
-	ClientSecret        *string                  `json:"clientSecret"`
-	Order               int32                    `json:"order"`
-	Active              bool                     `json:"active"`
-	TestEnvironment     bool                     `json:"testEnvironment"`
-	NotifUser           *string                  `json:"notifUser"`
-	NotifPassword       *string                  `json:"notifPassword"`
-	SoftDescriptor      *string                  `json:"softDescriptor"`
-	GatewayProcessID    int32                    `json:"gatewayProcessId"`
-	WebhookUrl          *string                  `json:"webhookUrl"`
-	Url                 *string                  `json:"url"`
-	AuthType            string                   `json:"authType"`
-	Use3ds              bool                     `json:"use3ds"`
-	AdqCode3ds          *string                  `json:"adqCode3ds"`
-	DefaultAdqCode      *string                  `json:"defaultAdqCode"`
-	UseAntifraud        bool                     `json:"useAntifraud"`
-	CreatedBy           int32                    `json:"createdBy"`
-	UpdatedBy           *int32                   `json:"updatedBy"`
-	CreatedAt           time.Time                `json:"createdAt"`
-	UpdatedAt           time.Time                `json:"updatedAt"`
+	ID                  int32                    `json:"id" example:"1"`
+	Uuid                uuid.UUID                `json:"uuid" example:"78e5259f-1b4e-460c-90d2-83e640a7d024"`
+	Name                string                   `json:"name" example:"Stripe"`
+	Description         *string                  `json:"description" example:"Payment gateway for Stripe"`
+	ClientID            *string                  `json:"clientId" example:"ZYDPLLBWSK3MVQJSIYHB1OR2JXCY0X2C5UJ2QAR2MAAIT5Q"`
+	ClientSecret        *string                  `json:"clientSecret" example:"52caa7c6-d107-42bd-afac-0226a501e66e"`
+	Order               int32                    `json:"order" example:"1"`
+	Active              bool                     `json:"active" example:"true"`
+	TestEnvironment     bool                     `json:"testEnvironment" example:"false"`
+	NotifUser           *string                  `json:"notifUser" example:"notification_user"`
+	NotifPassword       *string                  `json:"notifPassword" example:"notification_password"`
+	SoftDescriptor      *string                  `json:"softDescriptor" example:"Company Name"`
+	GatewayProcessID    int32                    `json:"gatewayProcessId" example:"1"`
+	WebhookUrl          *string                  `json:"webhookUrl" example:"https://example.com/webhook"`
+	Url                 *string                  `json:"url" example:"https://api.stripe.com"`
+	AuthType            string                   `json:"authType" example:"bearer"`
+	Use3ds              bool                     `json:"use3ds" example:"false"`
+	AdqCode3ds          *string                  `json:"adqCode3ds" example:"123456"`
+	DefaultAdqCode      *string                  `json:"defaultAdqCode" example:"654321"`
+	UseAntifraud        bool                     `json:"useAntifraud" example:"true"`
+	CreatedBy           int32                    `json:"createdBy" example:"1"`
+	UpdatedBy           *int32                   `json:"updatedBy" example:"1"`
+	CreatedAt           time.Time                `json:"createdAt" example:"2023-01-01T00:00:00Z"`
+	UpdatedAt           time.Time                `json:"updatedAt" example:"2023-01-01T00:00:00Z"`
 	GatewayProcess      GatewayProcessDTO        `json:"gatewayProcess"`
 	GatewayPaymentTypes []GatewayPaymentTypesDTO `json:"gatewayPaymentTypes"`
 }
 
 type GetOneByIdResDTO struct {
-	ID                  int32                    `json:"id"`
-	Uuid                uuid.UUID                `json:"uuid"`
-	Name                string                   `json:"name"`
-	Description         *string                  `json:"description"`
-	ClientID            *string                  `json:"clientId"`
-	ClientSecret        *string                  `json:"clientSecret"`
-	Order               int32                    `json:"order"`
-	Active              bool                     `json:"active"`
-	TestEnvironment     bool                     `json:"testEnvironment"`
-	NotifUser           *string                  `json:"notifUser"`
-	NotifPassword       *string                  `json:"notifPassword"`
-	SoftDescriptor      *string                  `json:"softDescriptor"`
-	GatewayProcessID    int32                    `json:"gatewayProcessId"`
-	WebhookUrl          *string                  `json:"webhookUrl"`
-	Url                 *string                  `json:"url"`
-	AuthType            string                   `json:"authType"`
-	Use3ds              bool                     `json:"use3ds"`
-	AdqCode3ds          *string                  `json:"adqCode3ds"`
-	DefaultAdqCode      *string                  `json:"defaultAdqCode"`
-	UseAntifraud        bool                     `json:"useAntifraud"`
-	CreatedBy           int32                    `json:"createdBy"`
-	UpdatedBy           *int32                   `json:"updatedBy"`
-	CreatedAt           time.Time                `json:"createdAt"`
-	UpdatedAt           time.Time                `json:"updatedAt"`
+	ID                  int32                    `json:"id" example:"1"`
+	Uuid                uuid.UUID                `json:"uuid" example:"78e5259f-1b4e-460c-90d2-83e640a7d024"`
+	Name                string                   `json:"name" example:"Stripe"`
+	Description         *string                  `json:"description" example:"Payment gateway for Stripe"`
+	ClientID            *string                  `json:"clientId" example:"ZYDPLLBWSK3MVQJSIYHB1OR2JXCY0X2C5UJ2QAR2MAAIT5Q"`
+	ClientSecret        *string                  `json:"clientSecret" example:"52caa7c6-d107-42bd-afac-0226a501e66e"`
+	Order               int32                    `json:"order" example:"1"`
+	Active              bool                     `json:"active" example:"true"`
+	TestEnvironment     bool                     `json:"testEnvironment" example:"false"`
+	NotifUser           *string                  `json:"notifUser" example:"notification_user"`
+	NotifPassword       *string                  `json:"notifPassword" example:"notification_password"`
+	SoftDescriptor      *string                  `json:"softDescriptor" example:"Company Name"`
+	GatewayProcessID    int32                    `json:"gatewayProcessId" example:"1"`
+	WebhookUrl          *string                  `json:"webhookUrl" example:"https://example.com/webhook"`
+	Url                 *string                  `json:"url" example:"https://api.stripe.com"`
+	AuthType            string                   `json:"authType" example:"bearer"`
+	Use3ds              bool                     `json:"use3ds" example:"false"`
+	AdqCode3ds          *string                  `json:"adqCode3ds" example:"123456"`
+	DefaultAdqCode      *string                  `json:"defaultAdqCode" example:"654321"`
+	UseAntifraud        bool                     `json:"useAntifraud" example:"true"`
+	CreatedBy           int32                    `json:"createdBy" example:"1"`
+	UpdatedBy           *int32                   `json:"updatedBy" example:"1"`
+	CreatedAt           time.Time                `json:"createdAt" example:"2023-01-01T00:00:00Z"`
+	UpdatedAt           time.Time                `json:"updatedAt" example:"2023-01-01T00:00:00Z"`
 	GatewayProcess      GatewayProcessDTO        `json:"gatewayProcess"`
 	GatewayPaymentTypes []GatewayPaymentTypesDTO `json:"gatewayPaymentTypes"`
 }
@@ -154,11 +154,11 @@ type SoftDeleteDTO struct {
 }
 
 type GatewayProcessDTO struct {
-	Name string `json:"name"`
-	ID   int32  `json:"id"`
+	Name string `json:"name" example:"Authorize Only"`
+	ID   int32  `json:"id" example:"2"`
 }
 
 type GatewayPaymentTypesDTO struct {
-	Name string `json:"name"`
-	ID   int32  `json:"id"`
+	Name string `json:"name" example:"Installment"`
+	ID   int32  `json:"id" example:"1"`
 }
