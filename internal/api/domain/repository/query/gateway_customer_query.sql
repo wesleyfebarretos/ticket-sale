5fb57ad22ce7a1c108fa6c5e2461e7e4c89374b9
Wesley Ferreira <wesley.ferreira@fengbrasil.com.br>
2024-08-15 23:29:38 -0300

    feat: gateway customer connection

---
+++ internal/api/domain/repository/query/gateway_customer_query.sql
@@ -0,0 +1,25 @@
+-- name: Create :one
+INSERT INTO fin.gateway_customer
+(user_id, gateway_id, gateway_customer_id)
+VALUES
+($1, $2, $3)
+RETURNING *;
+
+-- name: FindOneByUserId :one
+SELECT
+    *
+FROM
+    fin.gateway_customer
+WHERE
+    user_id = $1;
+
+-- name: FindOneByGatewayAndCustomerId :one
+SELECT
+    *
+FROM
+    fin.gateway_customer
+WHERE
+    user_id = $1
+AND
+    gateway_id = $2;
+
